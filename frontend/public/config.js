e98ac2421f4e1967b7115849c46502b8835022fa
Joao Eduardo Santos Pollhuber <jpollhuber@outlook.com>
2025-11-03 20:04:47 -0800

    Move backend url to separate config file

---
+++ frontend/public/config.js
@@ -0,0 +1,4 @@
+// Backend URL configuration
+// This file centralizes the backend URL to avoid duplication across the application
+window.BACKEND_URL = 'https://dj-clownfish-uxa88.ondigitalocean.app';
+
